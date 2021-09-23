fa4b8a5260c29890841112f5afa448b23d994f4b
Christine Yoo <christinecho.s@gmail.com>
2021-09-22 18:25:45 -0700

    added Context in a class component

---
+++ src/context/MovieList.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import UserContext from './userContext';
+
+export default class MovieList extends Component {
+  static contextType = UserContext; //we're setting a static property on this class
+
+  componentDidMount() {
+    console.log('context', this.context);
+  }
+
+  render() {
+    return (
+      <UserContext.Consumer>
+        {(userContext) => <div>Movie List {userContext.name}</div>}
+      </UserContext.Consumer>
+    );
+  }
+}
+
+// MovieList.contextType = UserContext; //this is setting it explicitly
---
+++ src/context/userContext.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const UserContext = React.createContext(); //returns a context object //use PascalNotation
+UserContext.displayName = "UserContext";
+
+export default UserContext;
